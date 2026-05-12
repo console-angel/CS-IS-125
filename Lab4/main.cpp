749c18be1f7fcb1a3a057f5fc2dab1a9df7c2b9e
Angel Gomez <g.angelpat7567@gmail.com>
2026-05-12 00:53:49 -0700

    Updated & Completed Lab 4

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -7,32 +7,36 @@
 
 int main(void)
 {
-    cout << "1. Do Catalan Numbers: " << endl;
-    cout << "2. Do Fibonacci Numbers: " << endl;
-    cout << "3. Q - Quit" << endl;
+    int choice = 0;
 
-    int choice;
-    cout << "Enter Selection: ";
-    cin >> choice;
+    do {
+        cout << "1. Do Catalan Numbers: " << endl;
+        cout << "2. Do Fibonacci Numbers: " << endl;
+        cout << "3. Q - Quit" << endl;
 
-    switch (choice)
-    {
-        case 1:
-            int n;
-            cout << "Enter n for Catalan number: ";
-            cin >> n;
-            cout << "The " << n << "th Catalan number is: " << catalan(n) << endl;
-            break;
-        case 2:
-            int m;
-            cout << "Enter n for Fibonacci number: ";
-            cin >> m;
-            cout << "The " << m << "th Fibonacci number is: " << fibonacci(m) << endl;
-            break;
-        case 3:
-            cout << "Quitting..." << endl;
-            break;
-        default:
-            cout << "Invalid selection. Please try again." << endl;
-    }
+        cout << "Enter Selection: ";
+        cin >> choice;
+        cout << endl;
+
+        switch (choice)
+        {
+            case 1:
+                int n;
+                cout << "Enter n for Catalan number: ";
+                cin >> n;
+                cout << "The " << n << "th Catalan number is: " << catalan(n) << endl << endl;
+                break;
+            case 2:
+                int m;
+                cout << "Enter n for Fibonacci number: ";
+                cin >> m;
+                cout << "The " << m << "th Fibonacci number is: " << fibonacci(m) << endl << endl;
+                break;
+            case 3:
+                cout << "Quitting..." << endl << endl;
+                break;
+            default:
+                cout << "Invalid selection. Please try again." << endl << endl;
+        }
+    } while (choice != 3);
 }
